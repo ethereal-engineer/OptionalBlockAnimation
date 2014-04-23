bb19e59ef76adf6cb5bcb197c8c5be757cb5a80d
Adam Iredale <adam@bionicmonocle.com>
2014-04-23 19:05:03 +1000

    Add support for a few more methods to complete the suite and rename the .m file

--- Classes/UIView+OptionalBlockAnimation.h
+++ Classes/UIView+OptionalBlockAnimation.h
@@ -1,6 +1,13 @@
+//
+//  UIView+OptionalBlockAnimation.h
+//
+//  Created by Adam Iredale on 28/03/2014.
+//  Copyright (c) 2014 Bionic Monocle Pty Ltd. All rights reserved.
+//
 
 /**
- *  A category on UIView that allows boolean control of animation for those pesky `animated:` methods
+ *  A category on UIView that allows boolean control of animation for those pesky `animated:` methods. If we are animating then
+ *  everything will run as normal. If not, the animation and completion blocks are called immediately instead.
  */
 
 @interface UIView(OBA_OptionalBlockAnimation)
@@ -12,5 +19,11 @@
 + (void)OBA_animate:(BOOL)animated withDuration:(NSTimeInterval)duration animations:(void (^)(void))animations NS_AVAILABLE_IOS(4_0);
 
 + (void)OBA_animate:(BOOL)animated withDuration:(NSTimeInterval)duration delay:(NSTimeInterval)delay usingSpringWithDamping:(CGFloat)dampingRatio initialSpringVelocity:(CGFloat)velocity options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(7_0);
+
++ (void)OBA_transition:(BOOL)animated withView:(UIView *)view duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(4_0);
+
++ (void)OBA_transition:(BOOL)animated fromView:(UIView *)fromView toView:(UIView *)toView duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(4_0);
+
+//+ (void)OBA_performSystemAnimation:(BOOL)animated animation:(UISystemAnimation)animation onViews:(NSArray *)views options:(UIViewAnimationOptions)options animations:(void (^)(void))parallelAnimations completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(7_0);
 
 @end

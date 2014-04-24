22ffa89b5978f5ed15386fbe458c83b52103d4dd
Shaps Mohsenin <shaps80@me.com>
2014-04-25 00:02:39 +0100

    Updated to animated ? duration : 0 style

--- Classes/UIView+OptionalBlockAnimation.m
+++ Classes/UIView+OptionalBlockAnimation.m
@@ -11,125 +11,32 @@
 
 + (void)OBA_animate:(BOOL)animated withDuration:(NSTimeInterval)duration delay:(NSTimeInterval)delay options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion
 {
-    if (animated)
-    {
-        [self animateWithDuration:duration delay:delay options:options animations:animations completion:completion];
-    }
-    else
-    {
-        if (animations)
-        {
-            animations();
-        }
-        
-        if (completion)
-        {
-            completion(YES);
-        }
-    }
+    [self animateWithDuration:animated ? duration : 0 delay:delay options:options animations:animations completion:completion];
 }
 
 + (void)OBA_animate:(BOOL)animated withDuration:(NSTimeInterval)duration animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion
 {
-    if (animated)
-    {
-        [self animateWithDuration:duration animations:animations completion:completion];
-    }
-    else
-    {
-        if (animations)
-        {
-            animations();
-        }
-        
-        if (completion)
-        {
-            completion(YES);
-        }
-    }
+  [self animateWithDuration:animated ? duration : 0 animations:animations completion:completion];
 }
 
 + (void)OBA_animate:(BOOL)animated withDuration:(NSTimeInterval)duration animations:(void (^)(void))animations
 {
-    if (animated)
-    {
-        [self animateWithDuration:duration animations:animations];
-    }
-    else
-    {
-        if (animations)
-        {
-            animations();
-        }
-    }
+  [self animateWithDuration:animated ? duration : 0 animations:animations];
 }
 
 + (void)OBA_animate:(BOOL)animated withDuration:(NSTimeInterval)duration delay:(NSTimeInterval)delay usingSpringWithDamping:(CGFloat)dampingRatio initialSpringVelocity:(CGFloat)velocity options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion
 {
-    if (animated)
-    {
-        [self animateWithDuration:duration delay:delay usingSpringWithDamping:dampingRatio initialSpringVelocity:velocity options:options animations:animations completion:completion];
-    }
-    else
-    {
-        if (animations)
-        {
-            animations();
-        }
-        
-        if (completion)
-        {
-            completion(YES);
-        }
-    }
+  [self animateWithDuration:animated ? duration : 0 delay:delay usingSpringWithDamping:dampingRatio initialSpringVelocity:velocity options:options animations:animations completion:completion];
 }
 
 + (void)OBA_transition:(BOOL)animated withView:(UIView *)view duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(4_0)
 {
-    if (animated)
-    {
-        [self transitionWithView:view duration:duration options:options animations:animations completion:completion];
-    }
-    else
-    {
-        if (animations)
-        {
-            animations();
-        }
-        
-        if (completion)
-        {
-            completion(YES);
-        }
-    }
+  [self transitionWithView:view duration:animated ? duration : 0 options:options animations:animations completion:completion];
 }
 
 + (void)OBA_transition:(BOOL)animated fromView:(UIView *)fromView toView:(UIView *)toView duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(4_0)
 {
-    if (animated)
-    {
-        [self transitionFromView:fromView toView:toView duration:duration options:options completion:completion];
-    }
-    else
-    {
-        if (options & UIViewAnimationOptionShowHideTransitionViews)
-        {
-            // Just hide and show
-            [fromView setHidden:YES];
-            [toView setHidden:NO];
-        }
-        else
-        {
-            // Add and remove
-            [fromView.superview addSubview:toView];
-            [fromView removeFromSuperview];
-        }
-
-        if (completion)
-        {
-            completion(YES);
-        }
-    }
+  [self transitionFromView:fromView toView:toView duration:animated ? duration : 0 options:options completion:completion];
 }
 
 //+ (void)OBA_performSystemAnimation:(BOOL)animated animation:(UISystemAnimation)animation onViews:(NSArray *)views options:(UIViewAnimationOptions)options animations:(void (^)(void))parallelAnimations completion:(void (^)(BOOL finished))completion NS_AVAILABLE_IOS(7_0)
